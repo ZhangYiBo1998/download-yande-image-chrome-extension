9503c4e3c35509ad28bb3f44cc95c7cf9d9822d4
ZhangYiBo1998 <27582180@qq.com>
2024-08-04 23:14:12 +0800

    实现chrome浏览器下载图片

--- src/Popup.tsx
+++ src/Popup.tsx
@@ -1,58 +1,184 @@
-// 引入所需的库和组件
-// import { h, render } from "preact";
-// import { useEffect, useState } from "preact/hooks";
-
-import React, {useState} from "react";
+import React, { useEffect, useState } from "react";
 import ReactDOM from "react-dom/client";
 
 import "./Popup.scss";
 
 // 定义PopupProps接口
 interface PopupProps {
-	"download-yande-image-chrome-extension-title": string;
+	ruleStr: string;
 }
 
 // 定义弹出窗口组件
-function Popup(props: PopupProps | {} = {}) {
-	console.log("Popup props:", props);
+function Popup(props: PopupProps) {
+	// 弹出窗口标题
+	const ruleStr = props["ruleStr"] || "";
 
-	const [titleValue, setTitleValue] = useState("");
+	// 输入框内容
+	const [inputValue, setInputValue] = useState<string>("");
+	// 图片链接
+	const [hrefValue, setHrefValue] = useState<string>("");
+	// 页面标题
+	const [title, setTitle] = useState<string>("");
+	// 文件名
+	const [fileName, setFileName] = useState<string>(() => {
+		const ruleStr = props["ruleStr"] || "{title}";
 
-	const handleTitleChange = (e: any) => {
-		console.log(e.target?.value);
-		setTitleValue(e.target?.value);
+		return replacePlaceholders(ruleStr);
+	});
+	// 图片下载按钮是否可用
+	const [downloadBtnDisabled, setDownloadBtnDisabled] =
+		useState<boolean>(false);
+
+	// 输入框内容变化时
+	const handleTitleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+		const value = e.target.value || "";
+		setInputValue(value);
 	};
 
+	// 保存规则
 	const handleSave = () => {
 		chrome.storage.local.set({
-			"download-yande-image-chrome-extension-title": titleValue,
+			ruleStr: inputValue,
 		});
+
+		setFileName(replacePlaceholders(inputValue));
 	};
 
+	// 点击下载按钮
 	const handleDownload = () => {
-		console.log("download");
+		imgDownload(hrefValue, fileName);
 	};
+
+	// 替换规则中的占位符
+	function replacePlaceholders(inputString: string): string {
+		const date = new Date().toLocaleString();
+
+		// 使用正则表达式替换 {title} 和 {date}
+		return inputString.replace(/{title}/g, title).replace(/{date}/g, date);
+	}
+
+	// 下载图片
+	function imgDownload(
+		href: string,
+		fileName: string = new Date().toLocaleString()
+	) {
+		const aEle = document.createElement("a");
+		aEle.href = href;
+		aEle.download = fileName;
+		aEle.click();
+	}
+
+	// 注入到当前标签页的JS脚本函数
+	function scriptForImgDownload() {
+		const title = document.title.replaceAll(/[<>:"\/\\|?*#]/g, "");
+
+		const imgEle = document.querySelectorAll(
+			"#highres"
+		)[0] as HTMLAnchorElement;
+		if (imgEle) {
+			imgEle.target = "_blank";
+		}
+
+		const pngEle = document.querySelectorAll("#png")[0] as HTMLAnchorElement;
+		if (pngEle) {
+			pngEle.target = "_blank";
+		}
+
+		const href = (pngEle || imgEle).href;
+
+		return {
+			title,
+			href,
+		};
+	}
+
+	useEffect(() => {
+		setInputValue(ruleStr);
+
+		// 在扩展的后台JS脚本中获取网页元素的内容
+		chrome.tabs.query({ active: true, currentWindow: true }, (tabs) => {
+			const tabId: number = tabs[0].id as number;
+			try {
+				if (chrome.scripting) {
+					chrome.scripting
+						.executeScript({
+							target: { tabId: tabId },
+							func: scriptForImgDownload,
+						})
+						.then((props) => {
+							console.log("script injected", props);
+
+							// 存储图片链接和页面标题
+							setHrefValue((props[0].result.href as string) || "");
+							setTitle((props[0].result.title as string) || "");
+						});
+				} else {
+					chrome.tabs.executeScript(
+						tabId,
+						{
+							code: `(${scriptForImgDownload.toString()})()`,
+						},
+						(results) => {
+							if (chrome.runtime.lastError) {
+								console.error(chrome.runtime.lastError);
+								return;
+							}
+
+							console.log("script injected", results);
+
+							if (results && results.length > 0) {
+								setHrefValue((results[0].href as string) || "");
+								setTitle((results[0].title as string) || "");
+							}
+						}
+					);
+				}
+			} catch (e) {
+				setDownloadBtnDisabled(true);
+			}
+		});
+	}, []);
 
 	return (
-		<div>
-			yande.re 下载器 文件名規則：
-			<input
-				type="text"
-				title=""
-				placeholder="文件名規則"
-				value={titleValue}
-				onChange={handleTitleChange}
-			/>
-			<button onClick={handleSave}>保存规则</button>
-			<button onClick={handleDownload}>下载</button>
+		<div id="popupBox">
+			<div className="ruleBox" style={{ display: "none" }}>
+				<div className="discriptionBox">
+					<h3 className="title">yande.re图片下载器 文件名規則：</h3>
+					<div className="discription">
+						<p>{"{title}当前标签页标题"}</p>
+						<p>{"{date}当前时间，格式：YYYY-MM-DD HH:mm:ss"}</p>
+						<p>{"{id}图片id，如：12345678"}</p>
+					</div>
+				</div>
+
+				<div className="inputBox">
+					<input
+						type="text"
+						title=""
+						placeholder="文件名規則"
+						value={inputValue}
+						onChange={handleTitleChange}
+					/>
+					<button className="saveBtn" onClick={handleSave}>
+						保存规则
+					</button>
+				</div>
+			</div>
+			<div className={!downloadBtnDisabled ? "hidden" : ""}>
+				下载功能暂不可用，请使用最新版Chrome浏览器。
+			</div>
+			<button
+				className="downloadBtn"
+				onClick={handleDownload}
+				disabled={downloadBtnDisabled}
+			>
+				下载
+			</button>
 		</div>
 	);
 }
 
 // 加载配置并渲染弹出窗口
-chrome.storage.local.get(
-	["download-yande-image-chrome-extension-title"],
-	(props: any = {}) => {
-		ReactDOM.createRoot(document.body).render(<Popup {...props} />);
-	}
-);
+chrome.storage.local.get(["ruleStr"], (props: any = {}) => {
+	ReactDOM.createRoot(document.body).render(<Popup {...props} />);
+});
--- src/background.ts
+++ src/background.ts
@@ -1,9 +1,4 @@
 // 使用chrome.storage.local.get()方法获取扩展的本地存储数据，并根据需要设置默认值
-chrome.storage.local.get(['title'], (props: any) => {
-    console.log('chrome.storage.local.get()获取扩展的本地存储数据成功', props);
+chrome.storage.local.get(["ruleStr"], (props: any) => {
+	console.log("chrome.storage.local.get()获取扩展的本地存储数据成功", props);
 });
-
-console.log('background.ts文件已加载');
-
-// 导出空对象
-export { };
