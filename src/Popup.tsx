e63a1b9d653a832dc40edb1687b21ff2ad1facbd
ZhangYiBo1998 <27582180@qq.com>
2024-08-08 00:37:51 +0800

    精简代码-并删除多余权限

--- src/Popup.tsx
+++ src/Popup.tsx
@@ -1,179 +1,113 @@
-import React, { useEffect, useState } from "react";
+import React, {useEffect, useState} from "react";
 import ReactDOM from "react-dom/client";
 import "./Popup.scss";
 
-// 定义PopupProps接口
-interface PopupProps {
-	ruleStr: string;
-}
-
 // 定义弹出窗口组件
-function Popup(props: PopupProps) {
-	const [ruleStr, setRuleStr] = useState<string>(props["ruleStr"] || "{title}");
-	// 输入框内容
-	const [inputValue, setInputValue] = useState<string>("");
-	// 图片链接
-	const [hrefValue, setHrefValue] = useState<string>("");
-	// 页面标题
-	const [pageTitle, setPageTitle] = useState<string>("");
-	// 文件名
-	const [fileName, setFileName] = useState<string>("");
-	// 图片下载按钮是否可用
-	const [downloadBtnDisabled, setDownloadBtnDisabled] =
-		useState<boolean>(false);
-	const [loading, setLoading] = useState<boolean>(false);
+function Popup() {
+    // 图片链接
+    const [hrefValue, setHrefValue] = useState<string>("");
+    const [fileName, setFileName] = useState("");
+    // 图片下载按钮是否可用
+    const [downloadBtnDisabled, setDownloadBtnDisabled] = useState<boolean>(false);
+    const [loading, setLoading] = useState<boolean>(false);
 
-	// 输入框内容变化时
-	const handleTitleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-		const value = e.target.value || "";
-		setInputValue(value);
-	};
+    // 点击下载按钮
+    const handleDownload = () => {
+        if (loading || downloadBtnDisabled) {
+            return;
+        }
+        imgDownload(hrefValue);
+    };
 
-	// 保存规则
-	const handleSave = () => {
-		chrome.storage.local.set({
-			ruleStr: inputValue,
-		});
-		setRuleStr(inputValue);
-		setFileName(replacePlaceholders(inputValue, pageTitle));
-	};
+    // 下载图片
+    function imgDownload(href: string) {
 
-	// 点击下载按钮
-	const handleDownload = () => {
-		if(loading || downloadBtnDisabled){
-			return;
-		}
-		imgDownload(hrefValue);
-	};
+        setLoading(true)
+        chrome.downloads.download({
+            url: href, // 图片的URL
+            filename: fileName, // 下载后的文件名
+            saveAs: false // 是否显示“另存为”对话框
+        }, function (downloadId) {
+            console.log('Download started with ID:', downloadId);
+            setLoading(false)
+            window.close();
+        });
+    }
 
-	// 替换规则中的占位符
-	function replacePlaceholders(inputString: string, title: string): string {
-		const date = new Date().toLocaleString();
+    // 注入到当前标签页的JS脚本函数
+    function scriptForImgDownload() {
+        const title = document.title.replaceAll(/[<>:"\/\\|?*#]/g, "");
 
-		// 使用正则表达式替换 {title} 和 {date}
-		return inputString.replace(/{title}/g, title).replace(/{date}/g, date);
-	}
+        const imgEle = document.querySelectorAll("#highres")[0] as HTMLAnchorElement;
+        if (imgEle) {
+            imgEle.target = "_blank";
+        }
 
-	// 下载图片
-	function imgDownload(href: string) {
-		console.log("downloading...", href, fileName);
+        const pngEle = document.querySelectorAll("#png")[0] as HTMLAnchorElement;
+        if (pngEle) {
+            pngEle.target = "_blank";
+        }
 
-		setLoading(true)
-		chrome.downloads.download({
-			url: href, // 图片的URL
-			filename: fileName, // 下载后的文件名
-			saveAs: false // 是否显示“另存为”对话框
-		  }, function(downloadId) {
-			console.log('Download started with ID:', downloadId);
-			setLoading(false)
-			window.close();
-		  });
-	}
+        const href = (pngEle || imgEle).href;
 
-	// 注入到当前标签页的JS脚本函数
-	function scriptForImgDownload() {
-		const title = document.title.replaceAll(/[<>:"\/\\|?*#]/g, "");
+        return {
+            href,
+            title,
+        };
+    }
 
-		const imgEle = document.querySelectorAll(
-			"#highres"
-		)[0] as HTMLAnchorElement;
-		if (imgEle) {
-			imgEle.target = "_blank";
-		}
+    useEffect(() => {
 
-		const pngEle = document.querySelectorAll("#png")[0] as HTMLAnchorElement;
-		if (pngEle) {
-			pngEle.target = "_blank";
-		}
+        // 在扩展的后台JS脚本中获取网页元素的内容
+        chrome.tabs.query({active: true, currentWindow: true}, (tabs) => {
+            const tabId: number = tabs[0].id as number;
+            try {
+                if (chrome.scripting) {
+                    chrome.scripting
+                        .executeScript({
+                            target: {tabId: tabId},
+                            func: scriptForImgDownload,
+                        })
+                        .then((props) => {
+                            console.log("script injected", props);
+                            const {href = "" as string, title = "" as string} = props[0].result;
 
-		const href = (pngEle || imgEle).href;
+                            // 存储图片链接和页面标题
+                            setHrefValue(href);
+                            setFileName(title)
+                        });
+                } else {
+                    setDownloadBtnDisabled(true);
+                }
+            } catch (e) {
+                console.log("error: ", e);
+                setDownloadBtnDisabled(true);
+            }
+        });
+    }, []);
 
-		return {
-			title,
-			href,
-		};
-	}
+    useEffect(() => {
+        setDownloadBtnDisabled(!hrefValue);
+    }, [hrefValue]);
 
-	useEffect(() => {
-		setInputValue(ruleStr);
-
-		// 在扩展的后台JS脚本中获取网页元素的内容
-		chrome.tabs.query({ active: true, currentWindow: true }, (tabs) => {
-			const tabId: number = tabs[0].id as number;
-			try {
-				if (chrome.scripting) {
-					chrome.scripting
-						.executeScript({
-							target: { tabId: tabId },
-							func: scriptForImgDownload,
-						})
-						.then((props) => {
-							console.log("script injected", props);
-							const { href = "" as string, title = "" as string } = props[0].result;
-
-							// 存储图片链接和页面标题
-							setHrefValue(href);
-							setPageTitle(title);
-							setFileName(replacePlaceholders(ruleStr, title));
-						});
-				} else {
-					setDownloadBtnDisabled(true);
-				}
-			} catch (e) {
-				console.log("error: ", e);
-				setDownloadBtnDisabled(true);
-			}
-		});
-	}, []);
-
-	useEffect(() => {
-		setDownloadBtnDisabled(!hrefValue);
-	}, [hrefValue]);
-
-	return (
-		<div id="popupBox">
-			<div className="ruleBox hidden">
-				<div className="discriptionBox">
-					<h3 className="title">yande.re图片下载器 文件名規則：</h3>
-					<div className="discription">
-						<p>{"{title}当前标签页标题"}</p>
-						<p>{"{date}当前时间，格式：YYYY-MM-DD HH:mm:ss"}</p>
-						<p>{"{id}图片id，如：12345678"}</p>
-					</div>
-				</div>
-
-				<div className="inputBox">
-					<input
-						type="text"
-						title=""
-						placeholder="文件名規則"
-						value={inputValue}
-						onChange={handleTitleChange}
-					/>
-					<button className="saveBtn" onClick={handleSave}>
-						保存规则
-					</button>
-				</div>
-			</div>
-			<div
-				className={!downloadBtnDisabled ? "hidden" : ""}
-				style={{ textAlign: "center" }}
-			>
-				<p>当前页无法下载，请前往https://yande.re/post/show/*</p>
-				<p>若页面正确，请使用最新版Chrome浏览器。</p>
-			</div>
-			<button
-				className="downloadBtn"
-				onClick={handleDownload}
-				disabled={downloadBtnDisabled}
-			>
-				{loading ? '等待下载......' : '下载'}
-			</button>
-		</div>
-	);
+    return (
+        <div id="popupBox">
+            <div
+                className={!downloadBtnDisabled ? "hidden" : ""}
+                style={{textAlign: "center"}}
+            >
+                <p>当前页无法下载，请前往{chrome.i18n.getMessage("enable_download_path")}</p>
+                <p>若路径正确，请使用最新版Chrome浏览器。</p>
+            </div>
+            <button
+                className="downloadBtn"
+                onClick={handleDownload}
+                disabled={downloadBtnDisabled}
+            >
+                {loading ? '等待下载......' : '下载'}
+            </button>
+        </div>
+    );
 }
 
-// 加载配置并渲染弹出窗口
-chrome.storage.local.get(["ruleStr"], (props: any = {}) => {
-	ReactDOM.createRoot(document.body).render(<Popup {...props} />);
-});
+ReactDOM.createRoot(document.body).render(<Popup/>);
--- src/background.ts
+++ src/background.ts
@@ -1,4 +1,1 @@
-// 使用chrome.storage.local.get()方法获取扩展的本地存储数据，并根据需要设置默认值
-chrome.storage.local.get(["ruleStr"], (props: any) => {
-	console.log("chrome.storage.local.get()获取扩展的本地存储数据成功", props);
-});
+export {}
